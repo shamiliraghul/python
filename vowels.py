6935ae22de4a1bec02db0fdec84154b072fe60ce
shamiliraghul <50619855+shamiliraghul@users.noreply.github.com>
2019-05-15 10:21:48 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,5 @@
+chr=str(input())
+if(chr=='a' or chr=='e' or chr=='i' or chr=='o' or chr=='u' or chr=='A' or chr=='E' or chr=='I' or chr=='O' or chr=='U'):
+    print('Vowel')
+else:
+    print('Consonant')
