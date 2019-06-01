2bf561c0d33351fbb29ecd0e8d5428e01e888e5c
shamiliraghul <50619855+shamiliraghul@users.noreply.github.com>
2019-06-01 19:41:13 +0530

    Create queries.py

---
+++ queries.py
@@ -0,0 +1,8 @@
+import math
+import functools
+n,k=map(int,input().split())
+l=[int(i) for i in input().split()]
+for i in range(k):
+    c,d=map(int,input().split())
+    t=functools.reduce(math.gcd,l[c-1:d])
+    print(t)
