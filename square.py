cfdd9e0f03f5c602f2b56a5ba5cafadbdcc18970
shamiliraghul <50619855+shamiliraghul@users.noreply.github.com>
2019-06-10 22:23:40 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,12 @@
+A,B=map(int,input().split())
+C,D=map(int,input().split())
+E,F=map(int,input().split())
+G,S=map(int,input().split())
+i=D-B
+j=F-S
+k=E-C
+l=G-A
+if i==j== k==l:
+	print("yes")
+else:
+	print("no")
