9f8c008f80fa83cf593208161151dd076e0c1680
shamiliraghul <50619855+shamiliraghul@users.noreply.github.com>
2019-07-18 19:12:19 +0530

    Create print-1fornxtgrtterm.py

---
+++ print-1fornxtgrtterm.py
@@ -0,0 +1,10 @@
+n = int(input())
+x = list(map(int, input().split()))
+for i in range(len(x)):
+    if i == n-1:
+        print(-1, end="")
+    else:
+        if x[i] > x[i+1]:
+            print(x[i+1], end=" ")
+        else:
+            print(-1, end=" ")
